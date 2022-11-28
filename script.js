81cd3c27dca9e05700d9be7e0d18f01fc57a03b7
Laman Panakhova <118442384+Laman2005@users.noreply.github.com>
2022-11-28 20:17:47 +0400

    update

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
-let myName = 'Natalia';
-let myCity = 'Mexico City';
+//let myName = 'Natalia';
+//let myCity = 'Mexico City';
 
-console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+//console.log(`My name is ${myName}. My favorite city is ${myCity}.`)
+
+ent.write("<h2 style='color:blue;'>Javascript rules!</h2>);
+    ent.write("<hr/>");
